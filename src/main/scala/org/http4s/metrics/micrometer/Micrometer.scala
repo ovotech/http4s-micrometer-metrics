f916a947aa7bf348b438ddaf419b9a0ef5f581d9
Gavin Bisesi <gbisesi@teikametrics.com>
2019-09-19 09:07:28 -0400

    Expose a middleware constructor that allows reusing a `Reporter`
    Instead of creating a new one

--- src/main/scala/org/http4s/metrics/micrometer/Micrometer.scala
+++ src/main/scala/org/http4s/metrics/micrometer/Micrometer.scala
@@ -2,6 +2,7 @@ package org.http4s.metrics.micrometer
 
 import scala.concurrent.duration._
 
+import cats.FlatMap
 import cats.effect._
 import cats.implicits._
 
@@ -27,119 +28,124 @@ object Micrometer {
       registry: MeterRegistry,
       config: Config
   )(implicit F: Concurrent[F]): F[MetricsOps[F]] =
-    create[F](registry, config)
+    fromRegistry[F](registry, config)
 
-  def create[F[_]](
+  def fromRegistry[F[_]](
       registry: MeterRegistry,
       config: Config
   )(implicit F: Concurrent[F]): F[MetricsOps[F]] =
-    Reporter.fromRegistry[F](registry, config.prefix, config.tags).map { reporter =>
-      new MetricsOps[F] {
+    Reporter
+      .fromRegistry[F](registry, config.prefix, config.tags)
+      .map(fromReporter(_, config))
 
-        private def namespace(classifier: Option[String]): String = {
-          classifier
-            .map(_.takeWhile(_ != '[').trim)
-            .filter(_.nonEmpty)
-            .getOrElse("default")
-        }
+  def fromReporter[F[_]: FlatMap](
+      reporter: Reporter[F],
+      config: Config
+  ): MetricsOps[F] =
+    new MetricsOps[F] {
 
-        private def name(classifier: Option[String], key: String): String =
-          s"${namespace(classifier)}.$key"
+      private def namespace(classifier: Option[String]): String = {
+        classifier
+          .map(_.takeWhile(_ != '[').trim)
+          .filter(_.nonEmpty)
+          .getOrElse("default")
+      }
 
-        private def tags(classifier: Option[String]): Tags = {
-          config.tags and classifier
-            .collect {
-              case TagsReg(tagsString) if tagsString.trim.nonEmpty =>
-                tagsString
-                  .split(",")
-                  .collect {
-                    case TagReg(key, value) =>
-                      Tags.of(key, value)
-                  }
-                  .reduce(_ and _)
-            }
-            .getOrElse(Tags.empty)
+      private def name(classifier: Option[String], key: String): String =
+        s"${namespace(classifier)}.$key"
 
-        }
-
-        def increaseActiveRequests(classifier: Option[String]): F[Unit] =
-          reporter.gauge(name(classifier, "active-requests"), tags(classifier)).flatMap(_.increment)
-
-        def decreaseActiveRequests(classifier: Option[String]): F[Unit] =
-          reporter.gauge(name(classifier, "active-requests"), tags(classifier)).flatMap(_.decrement)
-
-        def recordHeadersTime(
-            method: Method,
-            elapsed: Long,
-            classifier: Option[String]
-        ): F[Unit] =
-          reporter
-            .timer(
-              name(classifier, "response-headers-time"),
-              tags(classifier) and methodTags(method)
-            )
-            .flatMap(_.record(elapsed.nanos))
-
-        def recordAbnormalTermination(
-            elapsed: Long,
-            terminationType: TerminationType,
-            classifier: Option[String]
-        ): F[Unit] = {
-          val terminationTags = terminationType match {
-            case Abnormal => Tags.of("termination", "abnormal")
-            case Error => Tags.of("termination", "error")
-            case Timeout => Tags.of("termination", "timeout")
+      private def tags(classifier: Option[String]): Tags = {
+        config.tags and classifier
+          .collect {
+            case TagsReg(tagsString) if tagsString.trim.nonEmpty =>
+              tagsString
+                .split(",")
+                .collect {
+                  case TagReg(key, value) =>
+                    Tags.of(key, value)
+                }
+                .reduce(_ and _)
           }
-
-          recordResponseTime(
-            classifier,
-            tags(classifier) and terminationTags,
-            elapsed
-          )
-        }
-        def recordTotalTime(
-            method: Method,
-            status: Status,
-            elapsed: Long,
-            classifier: Option[String]
-        ): F[Unit] = {
-          val statusTags = status.responseClass match {
-            case Status.Informational =>
-              Tags.of("status-code", "1xx")
-            case Status.Successful =>
-              Tags.of("status-code", "2xx")
-            case Status.Redirection =>
-              Tags.of("status-code", "3xx")
-            case Status.ClientError =>
-              Tags.of("status-code", "4xx")
-            case Status.ServerError =>
-              Tags.of("status-code", "5xx")
-          }
-          val allTags = tags(classifier) and
-            Tags.of("termination", "normal") and
-            statusTags and
-            methodTags(method)
-
-          recordResponseTime(
-            classifier,
-            allTags,
-            elapsed
-          )
-        }
-
-        private def recordResponseTime(
-            classifier: Option[String],
-            tags: Tags,
-            elapsed: Long
-        ): F[Unit] =
-          reporter
-            .timer(name(classifier, "response-time"), tags)
-            .flatMap(_.record(elapsed.nanos))
-
-        private def methodTags(method: Method): Tags =
-          Tags.of("method", method.name.toLowerCase)
+          .getOrElse(Tags.empty)
 
       }
-    }
 
+      def increaseActiveRequests(classifier: Option[String]): F[Unit] =
+        reporter.gauge(name(classifier, "active-requests"), tags(classifier)).flatMap(_.increment)
+
+      def decreaseActiveRequests(classifier: Option[String]): F[Unit] =
+        reporter.gauge(name(classifier, "active-requests"), tags(classifier)).flatMap(_.decrement)
+
+      def recordHeadersTime(
+          method: Method,
+          elapsed: Long,
+          classifier: Option[String]
+      ): F[Unit] =
+        reporter
+          .timer(
+            name(classifier, "response-headers-time"),
+            tags(classifier) and methodTags(method)
+          )
+          .flatMap(_.record(elapsed.nanos))
+
+      def recordAbnormalTermination(
+          elapsed: Long,
+          terminationType: TerminationType,
+          classifier: Option[String]
+      ): F[Unit] = {
+        val terminationTags = terminationType match {
+          case Abnormal => Tags.of("termination", "abnormal")
+          case Error => Tags.of("termination", "error")
+          case Timeout => Tags.of("termination", "timeout")
+        }
+
+        recordResponseTime(
+          classifier,
+          tags(classifier) and terminationTags,
+          elapsed
+        )
+      }
+      def recordTotalTime(
+          method: Method,
+          status: Status,
+          elapsed: Long,
+          classifier: Option[String]
+      ): F[Unit] = {
+        val statusTags = status.responseClass match {
+          case Status.Informational =>
+            Tags.of("status-code", "1xx")
+          case Status.Successful =>
+            Tags.of("status-code", "2xx")
+          case Status.Redirection =>
+            Tags.of("status-code", "3xx")
+          case Status.ClientError =>
+            Tags.of("status-code", "4xx")
+          case Status.ServerError =>
+            Tags.of("status-code", "5xx")
+        }
+        val allTags = tags(classifier) and
+          Tags.of("termination", "normal") and
+          statusTags and
+          methodTags(method)
+
+        recordResponseTime(
+          classifier,
+          allTags,
+          elapsed
+        )
+      }
+
+      private def recordResponseTime(
+          classifier: Option[String],
+          tags: Tags,
+          elapsed: Long
+      ): F[Unit] =
+        reporter
+          .timer(name(classifier, "response-time"), tags)
+          .flatMap(_.record(elapsed.nanos))
+
+      private def methodTags(method: Method): Tags =
+        Tags.of("method", method.name.toLowerCase)
+
+    }
 }
