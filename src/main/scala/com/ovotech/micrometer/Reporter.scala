79441295a5f6cba08446924b8543a749b88f2b27
Scala Steward <me@scala-steward.org>
2021-05-18 08:57:35 +0200

    Failed Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=series/3.x

--- src/main/scala/com/ovotech/micrometer/Reporter.scala
+++ src/main/scala/com/ovotech/micrometer/Reporter.scala
@@ -2,7 +2,6 @@ package com.ovotech.micrometer
 
 import java.util.concurrent.atomic.AtomicInteger
 
-import cats.effect.concurrent.Semaphore
 import cats.effect.implicits._
 import cats.effect.{Sync, Concurrent}
 import cats.implicits._
@@ -13,6 +12,7 @@ import io.micrometer.core.{instrument => micrometer}
 import scala.collection.mutable
 import scala.collection.JavaConverters._
 import scala.concurrent.duration._
+import cats.effect.std.Semaphore
 
 trait Reporter[F[_]] {
   def counter(name: String): F[Counter[F]] = counter(name, Tags.empty)
